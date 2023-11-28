a1013e2209a7483261c299d81b87dd08759f81f0
s1lent-dev <pareshdeshpande2178+github@gmail.com>
2023-11-29 01:21:38 +0530

    Iterative statements are learned

--- C++ Fundamentals/3. Control flow/Iteration.cpp
+++ C++ Fundamentals/3. Control flow/Iteration.cpp
@@ -1,12 +1,51 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main () {
+int main()
+{
 
     /*
-    There are 
+    There are majorly 3 types of loops
+
+    1. For loop
+    2. While loop
+    3. Do - while loop
+
     */
+
+    // For loop
+
+    int a = 0;
+    for (int i = 0; i <= 10; i++)
+    {
+        a += i;
+    }
+    cout << "The addition of numbers from 0 to 10 is: " << a << endl;
+
+    // While loop
+    int b = 0;
+    while (b < 10)
+    {
+        if (b % 2 == 0)
+        {
+            cout << b << " ";
+        }
+        b++;
+    }
+    cout << "\n";
+
+    // Do - whihle loop
+
+    int c = 0;
+    do
+    {
+        if (c % 2 == 1)
+        {
+            cout << c << " ";
+        }
+        c++;
+    } while (c < 10);
 
     return 0;
 }
