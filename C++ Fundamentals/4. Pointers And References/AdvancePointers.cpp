68285d678b10200534684273ecb2fde7099c3c6a
s1lent-dev <pareshdeshpande2178+github@gmail.com>
2023-12-01 19:55:15 +0530

    Opaque pointers are learned

--- C++ Fundamentals/4. Pointers And References/AdvancePointers.cpp
+++ C++ Fundamentals/4. Pointers And References/AdvancePointers.cpp
@@ -1,14 +1,50 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+#include "Opaque.h"
 
-int main() {
-    
+Opaque::Opaque()
+{
+    MessageConstructed();
+}
+
+Opaque::~Opaque()
+{
+    MessageDestructed();
+}
+
+void Opaque::MessageConstructed()
+{
+    Struct_ptr = new MyDataStruct;
+    cout << "My Name is: " << Struct_ptr->name << " And my age is: " << Struct_ptr->age << endl;
+}
+int main()
+{
+
     /*
     Advance concepts regarding pointers -->
 
     1. Opaque Pointer
     2. Smart Pointers and it's types
-    
+    3. Pointers to functions
+    4. Pointers to Pointers
+    5. Pointers to class, structs etc
+
     */
+
+    // Opaque Pointer -->
+
+    Opaque obj;
+
+    // Smart Pointer -->
+
+    /*
+    There are Four types of smart pointers -->
+    1. Unique Pointer
+    2. Shared Pointer
+    3. Weak pointer
+    4. Auto pointer
+
+    */
+
     return 0;
 }
